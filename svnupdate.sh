404ce4d44f51ddd0f89e3cf306c603d659b45316
Conor Randall <coolo1mc@gmail.com>
2016-09-01 11:25:19 +1000

    update svnupdate.sh to output update version correctly

--- svnupdate.sh
+++ svnupdate.sh
@@ -4,7 +4,7 @@ case $response in
 svn update
 zip -r Builds/LeeDroid_$(svn info | grep 'Revision: ' | cut -d\   -f2).zip boot.img extras kernel META-INF supersu system
 echo $(md5sum Builds/LeeDroid_$(svn info | grep 'Revision: ' | cut -d\   -f2).zip) > Builds/Leedroid_$(svn info | grep 'Revision: ' | cut -d\   -f2).zip.md5
-echo 'Updated to revision $(svn info | grep 'Revision: ' | cut -d\   -f2) and built zip in Builds/LeeDroid_$(svn info | grep 'Revision: ' | cut -d\   -f2).zip'
+echo 'Updated to' $(svn info | grep 'Revision') 'and built zip in Builds/LeeDroid_'$(svn info | grep 'Revision: ' | cut -d\   -f2)'.zip'
         ;;
     *)
         exit
